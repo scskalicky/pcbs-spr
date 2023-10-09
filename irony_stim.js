0959ca61a6607cead668575636ca954b43693e68
Stephen Skalicky <scskalicky@gmail.com>
2023-10-09 20:35:01 +1300

    Initial commit

---
+++ irony_stim.js
@@ -0,0 +1,140 @@
+var irony_stim_practice = [
+    // {
+    //   "situation": "Heather is late meeting up with Sam.<br>When she finally arrives, Sam says:<br><br> "<i>Right on time again Heather</i>."", data: {valence: "negative", condition: "ironic", practice: "yes"}
+    // },
+    {
+      "situation": 'Maggie is looking very smart in her new formal suit.<br>When her friend Simon sees her he says:<br><br><i>"You\'re a fashion queen, Maggie.</i>"', data: {valence: "positive", condition: "literal", practice: "yes"}
+    },
+    {
+      "situation": 'Peter has been trying to date Jenefer.<br>Jenefer has politely refused all his invitations but Peter carries on asking for a date. Jenefer says:<br><br>"<i>You never give up, do you</i>?"', data: {valence: "negative", condition: "literal",practice: "yes"}
+    }]
+   
+    var irony_stim_A = [ 
+    {
+      "situation": 'Tom has just got his end of year salary bonus. It was a lot more than he expected. He says to his boss:<br><br> "<i>Well, I can go ahead and buy that new car now</i>."',data:{stim_id: "01A", valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Martin and Mary lost a dance competition together. Gary wasn\'t sure if Mary was a good dancer so he asked his friend Martin. Martin says :<br><br>"<i>She\'s got no sense of rhythm at all</i>."', data: {stim_id: "02A", valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Tom is in a restaurant with his friend. He has ordered a pasta dish. When the meal comes it is a rice dish. He says to his friend:<br><br>"<i>Hey, this is just what I wanted</i>."', data: {stim_id: "03A", valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Simon and Tony support the same football team which has just lost its fifth match in a row. Simon says to Tony:<br><br> "<i>What a terrible team</i>."', data: {stim_id: "04A", valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Mary was acting in a play. Simon has just read a very critical review of the play in the local newspaper. Simon says to his friend Janice:<br><br> "<i>Mary must be a great actor</i>."', data: {stim_id: "05A", valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Mary used to have long hair but she has just had it cut short. Simon likes the way Mary looks now. He is talking to Bill and says:<br><br> "<i>Mary\'s new hair style looks great</i>."', data:{stim_id: "06A",valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Sam has just bought a new suit. It is a very bright pink. Tom likes Sam\'s suit and he says to Sam:<br><br> "<i>Hey, you need a bit more colour in your clothes you know</i>."', data:{stim_id: "07A",valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Sue\'s exam mark is much better than her friend Simon\'s mark. Simon says to Sue:<br><br> "<i>I see you did really well in the exam as usual</i>."', data:{stim_id: "08A",valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Simon is in the library - one of his favourite places. He finds the book he was looking for and goes to the librarian. He says:<br><br> "<i>This is a great library</i>."', data: {stim_id: "09A",valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Martin attended a lecture given by his friend Susan. After the lecture, Martin heard some nice comments about the lecture from a member of the audience. When Martin next saw Susan he said to her:<br><br> "<i>You really know how to bore an audience</i>."',data:{stim_id: "10A", valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Susan likes to invite her friends for a meal but everyone knows that she is not a very good cook. After eating a meal at Susan\'s apartment, Simon says to another friend:<br><br> "<i>It was an absolutely awful meal</i>."',data:{stim_id: "11A",valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Dave has just bought a new sports car and is taking his friend Sam for a drive. Dave drives very fast and Sam is impressed. Sam says to Dave:<br><br> "<i>This car is a bit on the slow side, isn\'t it</i>?"', data:{stim_id: "12A", valence: "positive",condition: "ironic",
+      practice: "no"}
+    },
+    {
+      "situation": 'Martin has moved into a new apartment. His friend Angela wanted to visit him and so he gave her some simple directions. But Angela manages to lose her way. When Martin sees Angela he says to her:<br><br> "<i>You have a really great sense of direction, don\'t you</i>?"', data:{stim_id: "13A", valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Simon is in the library looking for a book with the librarian. They find it in the wrong place on the shelves. Simon says to the librarian :<br><br> "<i>This is a great library, isn\'t it</i>?"', data:{stim_id: "14A", valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Martin has a reputation for always being late. Phil is talking to someone who does not know Martin very well, and Phil says:<br><br> "<i>He\'s not the most punctual person you know</i>."', data: {stim_id: "15A", valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Martin and Simon went to see their close friend Peter play football. They saw Peter score a goal and help his side to win. Martin said to Simon:<br><br> "<i>Peter\'s an absolutely useless football player</i>."', data:{stim_id: "16A", valence: "positive",condition: "ironic",
+      practice: "no"}
+    },
+    {
+      "situation": 'Peter was feeling sick but he went ahead and picked up his close friend, Chris, to take him to college as he had promised. Later that day Chris said to his friend:<br><br> "<i>You just can\'t trust Peter to keep his promise</i>."', data: {stim_id: "17A", valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Mary is a very popular girl and everybody likes her a lot. Sam is talking about Mary and says:<br><br> "<i>She\'s a really kind person you know</i>."', data:{stim_id: "18A", valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Gary goes to the gym three times a week to work out for at least an hour each time. He looks skinny and is often exhausted. When Martin sees Gary he says:<br><br> "<i>You are not looking so good these days</i>."', data: {stim_id: "19A", valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Mary has just had her hair cut short. Both Bill and Simon like Mary with long hair. Bill says to Simon:<br><br> "<i>Mary\'s new hair style looks just great</i>."', data:{stim_id: "20A", valence: "negative",condition: "ironic",practice: "no"}
+    }
+  ]
+
+var irony_stim_B = [ 
+    {
+      "situation": 'Tom has just got his end of year salary bonus. It was a lot less than he expected. He says to his boss:<br><br> "<i>Well, I can go ahead and buy that new car now</i>."',data:{stim_id: "01B", valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Martin and Mary won a dance competition together. Gary wasn\'t sure if Mary was a good dancer so he asked his friend Martin. Martin says :<br><br>"<i>She\'s got no sense of rhythm at all</i>."', data: {stim_id: "02B", valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Tom is in a restaurant with his friend. He has ordered a pasta dish. When the meal comes it is a pasta dish. He says to his friend:<br><br>"<i>Hey, this is just what I wanted</i>."', data: {stim_id: "03B", valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Simon and Tony support the same football team which has just won its fifth match in a row. Simon says to Tony:<br><br> "<i>What a terrible team</i>."', data: {stim_id: "04B", valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Mary was acting in a play. Simon has just read a very flattering review of the play in the local newspaper. Simon says to his friend Janice:<br><br> "<i>Mary must be a great actor</i>."', data: {stim_id: "05B", valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Mary used to have long hair but she has just had it cut short. Simon dislikes the way Mary looks now. He is talking to Bill and says:<br><br> "<i>Mary\'s new hair style looks great</i>."', data:{stim_id: "06B",valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Sam has just bought a new suit. It is a very dull grey. Tom dislikes Sam\'s suit and he says to Sam:<br><br> "<i>Hey, you need a bit more colour in your clothes you know</i>."', data:{stim_id: "07B",valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Sue\'s exam mark is much worse than her friend Simon\'s mark. Simon says to Sue:<br><br> "<i>I see you did really well in the exam as usual</i>."', data:{stim_id: "08B",valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Simon is in the library - one of his favourite places. He cannot find the book he was looking for and goes to the librarian. He says :<br><br> "<i>This is a great library</i>."', data: {stim_id: "09B",valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Martin attended a lecture given by his friend Susan. After the lecture, Martin heard some mean comments about the lecture from a member of the audience. When Martin next saw Susan he said to her:<br><br> "<i>You really know how to bore an audience</i>."',data:{stim_id: "10B", valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Susan likes to invite her friends for a meal and everyone knows that she is a very good cook. After eating a meal at Susan\'s apartment, Simon says to another friend:<br><br> "<i>It was an absolutely awful meal</i>."',data:{stim_id: "11B",valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Dave has just bought a new sports car and is taking his friend Sam for a drive. Dave drives very slow and Sam is not impressed. Sam says to Dave:<br><br> "<i>This car is a bit on the slow side, isn\'t it</i>?"', data:{stim_id: "12B", valence: "negative",condition: "literal",
+      practice: "no"}
+    },
+    {
+      "situation": 'Martin has moved into a new apartment. His friend Angela wanted to visit him and so he gave her some complicated directions. But Angela manages to find her way. When Martin sees Angela he says to her:<br><br> "<i>You have a really great sense of direction, don\'t you</i>?"', data:{stim_id: "13B", valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Simon is in the library looking for a book with the librarian. They find it in the right place on the shelves. Simon says to the librarian :<br><br> "<i>This is a great library, isn\'t it</i>?"', data:{stim_id: "14B", valence: "positive",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Martin has a reputation for never being late. Phil is talking to someone who does not know Martin very well, and Phil says:<br><br> "<i>He\'s not the most punctual person you know</i>."', data: {stim_id: "15B", valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Martin and Simon went to see their close friend Peter play football. They saw Peter miss a goal and cause his side to lose. Martin said to Simon:<br><br> "<i>Peter\'s an absolutely useless football player</i>."', data:{stim_id: "16B", valence: "negative",condition: "literal",
+      practice: "no"}
+    },
+    {
+      "situation": 'Peter was feeling sick so he did not pick up his close friend, Chris, to take him to college as he had promised. Later that day Chris said to his friend: <br><br> "<i>You just can\'t trust Peter to keep his promise</i>."', data: {stim_id: "17B", valence: "negative",condition: "literal",practice: "no"}
+    },
+    {
+      "situation": 'Mary is a very unpopular girl and everybody dislikes her a lot. Sam is talking about Mary and says:<br><br> "<i>She\'s a really kind person you know</i>."', data:{stim_id: "18B", valence: "negative",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Gary goes to the gym three times a week to work out for at least an hour each time. He looks fit and is often energetic. When Martin sees Gary he says:<br><br> "<i>You are not looking so good these days</i>."', data: {stim_id: "19B", valence: "positive",condition: "ironic",practice: "no"}
+    },
+    {
+      "situation": 'Mary has just had her hair cut short. Both Bill and Simon like Mary with short hair. Bill says to Simon:<br><br> "<i>Mary\'s new hair style looks just great</i>."', data:{stim_id: "20B", valence: "positive",condition: "literal",practice: "no"}
+    }
+  ]
---
+++ pcbs.js
@@ -0,0 +1,191 @@
+const pigs = [
+    {
+    s: "Three Little Pigs\n" +
+    "Once there were three little pigs who lived together \n" +
+    "in mutual respect and in harmony with their environment.\n" +
+    "Using materials that were indigenous to the area, they\n" +
+    "each built a beautiful house. One pig built a house of straw,\n"+
+    "one a house of sticks, and one a house of dung, clay, and\n"+
+    "creeper vines shaped into bricks and baked in a small kiln.\n" +
+    "When they were finished, the pigs were satisfied with their\n" +
+    "work and settled back to live in peace and self-determination."
+},
+{
+    s: "But their idyll was soon shattered. One day, along came\n" +
+    "a big, bad wolf with expansionist ideas. He saw the pigs\n" +
+    "and grew very hungry in both a physical and an ideological\n" +
+    "sense. When the pigs saw the wolf, they ran into the house\n" +
+    "of straw. The wolf ran up to the house and banged on the\n" +
+    "door shouting, 'Little pigs, little pigs, let me in!'"
+}, 
+
+{
+    s: "The pigs shouted back, 'Your gunboat tactics hold\n" + 
+        "no fear for pigs defending their homes and culture.'\n" + 
+        "But the wolf wasn't to be denied what he thought was\n"+
+        "his manifest destiny. So he huffed and puffed and blew\n" +
+        "down the house of straw. The frightened pigs ran to the \n" + 
+        "house of sticks, with the wolf in hot pursuit. Where the\n" + 
+        "house of straw had stood, other wolves bought up the land \n" +
+        "and started a banana plantation."
+},
+{
+s: "At the house of sticks, the wolf again banged on the door\n" +
+  "and shouted, 'Little pigs, little pigs, let me in!'\n" +
+   "The little pigs shouted back, 'Go to hell, you carnivorous,\n" +
+    "imperialistic oppressor!' At this, the wolf chuckled\n" +
+    "condescendingly. He thought to himself: 'They are so \n" +
+    "childlike in their ways. It will be a shame to see them go,\n" +
+    "but progress cannot be stopped.'"
+},
+{
+    s: "So the wolf huffed and puffed and blew down the house of\n" + 
+        "sticks. The pigs ran to the house of bricks, with the wolf\n" +
+        "close at their heels. Where the house of sticks had stood,\n" +
+        "other wolves built a time-share condo resort complex for\n" +
+        "vacationing wolves, with each unit a fiberglass reconstruction\n" +
+        "of the house of sticks, as well as native curio shops,\n" +
+        "snorkeling, and dolphin shows."
+},
+
+{
+    s: "At the house of bricks, the wolf again banged on the door\n" +
+       "and shouted, 'Little pigs, little pigs, let me in!' This time\n" +
+       "in response the little pigs sang songs of solidarity and\n" +
+       "wrote letters of protest to the United Nations."
+},
+
+{
+    s: "By now the wolf was getting angry at the pigs' refusal\n"+
+        "to see the situation from the carnivore's point of view.\n"+
+        "So he huffed and puffed, and huffed and puffed, then grabbed\n"+
+        "his chest and fell over dead of a massive heart attack brought\n"  +
+         "on by eating too many fatty foods."
+},
+
+{
+    s: "The three little pigs rejoiced that justice had triumphed\n" +
+        "and did a little dance around the corpse of the wolf.\n" +
+        "Their next step was to liberate their homeland.\n" + 
+        "They gathered together a band of other pigs who had been\n" + 
+        "forced off their lands. This new brigade of porcinistas\n" + 
+        "attacked the resort complex with machine guns and rocket\n" +
+        "launchers and slaughtered the cruel wolf oppressors, sending\n" +
+        "a clear signal to the rest of the hemisphere not to meddle\n" +
+        "in their internal affairs. Then the pigs set up a model\n" +
+        "socialist democracy with free education, universal \n" +
+        "health care, and affordable housing for everyone."
+},
+
+{
+    s: "Please note: The wolf in this story was a metaphorical\n" + 
+    "construct. No actual wolves were harmed in the writing\n" +
+    "of this story."
+}
+]
+
+
+const red = [
+    {
+        s: "Little Red Riding Hood\n" + 
+        "There was once a young person named Red Riding Hood\n" +
+        "who lived with her mother on the edge of a large wood.\n" +
+        "One day her mother asked her to take a basket of fresh fruit\n" +
+        "and mineral water to her grandmother's house - not because\n" +
+        "this was womyn's work, mind you, but because the deed was\n " +
+        "generous and helped engender a feeling of community.\n" +
+        "Furthermore, her grandmother was not sick, but rather was\n" +
+        "in full physical and mental health and was fully capable\n" +
+        "of taking care of herself as a mature adult."
+    },
+
+    {
+        s: "So Red Riding Hood set off with her basket through the woods.\n" + 
+        "Many people believed that the forest was a foreboding and\n" + 
+        "dangerous place and never set foot in it. Red Riding Hood,\n" + 
+        "however, was confident enough in her own budding sexuality\n" +
+        "that such obvious Freudian imaginery did not intimidate her.\n" +
+        "On the way to Grandma's house, Red Riding Hood was accosted\n" +
+        "by a wolf, who asked her what was in her basket. She replied,\n" 
+        +"'Some healthful snacks for my grandmother, who is certainly\n" +
+        "capable of taking care of herself as a mature adult.'"
+     },
+
+    {
+        s: "The wolf said, 'You know, my dear, it isn't safe for a little\n" +
+        "girl to walk through these woods alone.'\n" + 
+        "Red Riding Hood said, 'I find your sexist remark offensive\n" +
+        "in the extreme, but I will ignore it because of your\n" +
+        "traditional status as an outcast from society, the stress\n" +
+        "of which has caused you to develop your own, entirely valid,\n" +
+        "worldview. Now, if you'll excuse me, I must be on my way.'"
+    },
+    {
+        s: "Red Riding Hood walked on along the main path. But, because\n" +
+        "his status outside society has freed him from slavish\n" +
+        "adherence to linear, Western-style thought, the wolf knew a\n" +
+        "quicker route to Grandma's house. He burst into the house and\n" +
+        "ate Grandma, an entirely valid course of action for a carnivore\n" +
+        "such as himself. Then, unhampered by rigid, traditionalist\n" +
+        "notions of what was masculine or feminine, he put on\n" +
+        "Grandma's nightclothes and crawled into bed."
+    },
+
+    {
+        s: "Red Riding Hood entered the cottage and said, 'Grandma, I have\n" +
+        "brought you some fat-free, sodium-free snacks to salute you\n " +
+        "in your role of a wise and nurturing matriarch.'\n" + 
+        "From the bed, the wolf said softly, 'Come closer, child,\n" +
+        "so that I might see you.'\n" + 
+        "Red Riding Hood said, 'Oh, I forgot you are as optically\n" +
+        "challenged as a bat. Grandma, what big eyes you have!'" 
+    },
+{
+    s: "'They have seen much, and forgiven much, my dear.'"
+},
+
+{
+    s: "'Grandma, what a big nose you have - only relatively,\n" +
+    "of course, and certainly attractive in its own way'"
+},
+
+{
+    s: "'It has smelled much, and forgiven much, my dear.'"
+},
+
+{
+    s: "'Grandma, what big teeth you have!'\n"+
+    "The wolf said, 'I am happy with who I am and what I am,' and\n" +
+    "leaped out of bed. He grabbed Red Riding Hood in his claws,\n" +
+    "intent on devouring her. Red Riding Hood screamed, not out of\n " +
+    "alarm at the wolf's apparent tendency toward cross-dressing,\n" + 
+    "but because of his willful invasion of her personal space."
+},
+
+{
+    s: "Her screams were heard by a passing woodchopper person \n" +
+    "(or log-fuel technician, as he preferred to be called). When\n" +
+    "he burst into the cottage, he saw melee there and tried to\n" +
+    "intervene. But as he raised his ax, Red Riding Hood and the\n" +
+    "wolf both stopped. 'And just what do you think you're doing?'\n" +
+    "asked Red Riding Hood. The woodchopper person blinked and\n " +
+    "tried to answer, but no words came to him."
+
+},
+{
+    s: "'Bursting in here like a Neanderthal, trusting your weapon to\n" +
+    "do your thinking for you!' she exclaimed. 'Sexist! Speciesist!\n" +
+    "How dare you assume that womyn and wolves can't solve their\n" +
+    "own problems without a man's help!'"
+},
+{
+    s: "When she heard Red Riding Hood's impassioned speech, Grandma\n" +
+        "jumped out of the wolf's mouth, took the woodchopper person's\n" +
+    "ax, and cut his head off. After this ordeal, Red Riding Hood,\n" +
+    "Grandma, and the Wolf felt a certain commonality of purpose.\n" +
+    "They decided to set up an alternative household based on\n" +
+    "mutual respect and cooperation, and they lived together\n" +
+    "in the woods happily ever after." 
+
+}
+]
